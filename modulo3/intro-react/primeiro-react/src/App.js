a75d3bd70e912f5ea5a21cf11eb26edfcd10f316
Carlos Roberto Fernandes Torres <crftorres@hotmail.com>
2022-06-22 00:00:55 -0300

    Primeiro projeto usando react nunca se esquece

---
+++ modulo3/intro-react/primeiro-react/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import logotipoLuminus from './imagens/Luminus_Eletricidade.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1> Luminus eletricidade e engenharia </h1>
+        <img src={logotipoLuminus} alt="Logotipo da Luminus eletricidade e engenharia" />
+    </div>
+  );
+}
+
+export default App;
