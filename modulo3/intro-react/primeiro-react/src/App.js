82377fb7a7793d627ebb68eb21175b9ce9bd9fda
Carlos Roberto Fernandes Torres <crftorres@hotmail.com>
2022-06-23 20:22:11 -0300

    comecando react

--- modulo3/intro-react/primeiro-react/src/App.js
+++ modulo3/intro-react/primeiro-react/src/App.js
@@ -2,11 +2,18 @@
 import logotipoLuminus from './imagens/Luminus_Eletricidade.png'
 import './App.css';
 
-function App() {
+function App() 
+{
+  function mensagemsistema()
+  {
+    alert("Olá mundo do react, vou te dominar!")
+  }
+
   return (
     <div className="App">
         <h1> Luminus eletricidade e engenharia </h1>
         <img src={logotipoLuminus} alt="Logotipo da Luminus eletricidade e engenharia" />
+        <button onClick={mensagemsistema}> Clique para ver a mensagem! </button>
     </div>
   );
 }
