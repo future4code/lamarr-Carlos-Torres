459abc648463bb764eb56b26cef1f351ac699efe
Carlos Roberto Fernandes Torres <crftorres@hotmail.com>
2022-10-20 23:55:43 -0300

    Projeto lista typescript exercicio 2

---
+++ modulo5/Lista-typescript/Exercicio2/src/index.ts
@@ -0,0 +1,19 @@
+console.log("Hello world!")
+
+function MostraTipoVariavel(VariavelEsperada: any) {
+
+    console.log(typeof(VariavelEsperada))
+
+}
+
+MostraTipoVariavel("Carlos")
+
+let minhaData = new Date()
+MostraTipoVariavel(minhaData)
+
+MostraTipoVariavel(1200)
+
+let Vetor = [10,20,30]
+
+MostraTipoVariavel(Vetor)
+
